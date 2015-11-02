503c069d4016918f7c35bd81d06d6b120562fc56
Antoine Grandiere <grandiere.antoine@yahoo.com>
2015-11-02 09:59:43 +0100

    Création School + Promotion

---
+++ Dev/SchoolIn/Promotion.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolIn
+{
+    public class Promotion
+    {
+        string _name;
+        int _nbstudent;
+
+        public Promotion(string name)
+        {
+            if (string.IsNullOrWhiteSpace(name))
+            {
+                throw new ArgumentException();
+            }
+            _name = name;
+        }
+
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+
+        public int NbStudent
+        {
+            get { return _nbstudent; }
+            set { _nbstudent = value; }
+        }
+    }
+}
