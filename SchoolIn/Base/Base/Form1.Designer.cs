b0a6fd48b33d854e30ca5bced6c416751f321ec8
Victor <victor.andre900@gmail.com>
2015-12-08 12:13:23 +0100

    gestion git

--- SchoolIn/Base/Base/Form1.Designer.cs
+++ SchoolIn/Base/Base/Form1.Designer.cs
@@ -35,15 +35,17 @@
             System.Windows.Forms.Calendar.CalendarHighlightRange calendarHighlightRange5 = new System.Windows.Forms.Calendar.CalendarHighlightRange();
             this.tabControl1 = new System.Windows.Forms.TabControl();
             this.page_promotion = new System.Windows.Forms.TabPage();
-            this.promotion_page1 = new Base.Promotion_page();
             this.tabPage2 = new System.Windows.Forms.TabPage();
-            this.teacher_page1 = new Base.Teacher_page();
             this.tabPage1 = new System.Windows.Forms.TabPage();
-            this.students_page1 = new Base.Students_page();
             this.Classroom_page = new System.Windows.Forms.TabPage();
-            this.classroomPage1 = new Base.ClassroomPage();
             this.tabPage3 = new System.Windows.Forms.TabPage();
             this.calendar1 = new System.Windows.Forms.Calendar.Calendar();
+            this.Listbox_Teacher = new System.Windows.Forms.ListBox();
+            this.Refresh_Listbox_Teacher = new System.Windows.Forms.Button();
+            this.promotion_page1 = new Base.Promotion_page();
+            this.teacher_page1 = new Base.Teacher_page();
+            this.students_page1 = new Base.Students_page();
+            this.classroomPage1 = new Base.ClassroomPage();
             this.header = new Base.Header();
             this.tabControl1.SuspendLayout();
             this.page_promotion.SuspendLayout();
@@ -66,7 +68,7 @@
             this.tabControl1.Location = new System.Drawing.Point(13, 118);
             this.tabControl1.Name = "tabControl1";
             this.tabControl1.SelectedIndex = 0;
-            this.tabControl1.Size = new System.Drawing.Size(678, 399);
+            this.tabControl1.Size = new System.Drawing.Size(876, 399);
             this.tabControl1.TabIndex = 1;
             // 
             // page_promotion
@@ -75,18 +77,10 @@
             this.page_promotion.Location = new System.Drawing.Point(4, 22);
             this.page_promotion.Name = "page_promotion";
             this.page_promotion.Padding = new System.Windows.Forms.Padding(3);
-            this.page_promotion.Size = new System.Drawing.Size(670, 373);
+            this.page_promotion.Size = new System.Drawing.Size(868, 373);
             this.page_promotion.TabIndex = 0;
             this.page_promotion.Text = "Promotion";
             this.page_promotion.UseVisualStyleBackColor = true;
-            // 
-            // promotion_page1
-            // 
-            this.promotion_page1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.promotion_page1.Location = new System.Drawing.Point(3, 3);
-            this.promotion_page1.Name = "promotion_page1";
-            this.promotion_page1.Size = new System.Drawing.Size(664, 367);
-            this.promotion_page1.TabIndex = 0;
             // 
             // tabPage2
             // 
@@ -94,18 +88,10 @@
             this.tabPage2.Location = new System.Drawing.Point(4, 22);
             this.tabPage2.Name = "tabPage2";
             this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage2.Size = new System.Drawing.Size(670, 373);
+            this.tabPage2.Size = new System.Drawing.Size(868, 373);
             this.tabPage2.TabIndex = 1;
             this.tabPage2.Text = "Teachers";
             this.tabPage2.UseVisualStyleBackColor = true;
-            // 
-            // teacher_page1
-            // 
-            this.teacher_page1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.teacher_page1.Location = new System.Drawing.Point(3, 3);
-            this.teacher_page1.Name = "teacher_page1";
-            this.teacher_page1.Size = new System.Drawing.Size(664, 367);
-            this.teacher_page1.TabIndex = 0;
             // 
             // tabPage1
             // 
@@ -113,18 +99,10 @@
             this.tabPage1.Location = new System.Drawing.Point(4, 22);
             this.tabPage1.Name = "tabPage1";
             this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage1.Size = new System.Drawing.Size(670, 373);
+            this.tabPage1.Size = new System.Drawing.Size(868, 373);
             this.tabPage1.TabIndex = 2;
             this.tabPage1.Text = "Students";
             this.tabPage1.UseVisualStyleBackColor = true;
-            // 
-            // students_page1
-            // 
-            this.students_page1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.students_page1.Location = new System.Drawing.Point(3, 3);
-            this.students_page1.Name = "students_page1";
-            this.students_page1.Size = new System.Drawing.Size(664, 367);
-            this.students_page1.TabIndex = 0;
             // 
             // Classroom_page
             // 
@@ -132,33 +110,28 @@
             this.Classroom_page.Location = new System.Drawing.Point(4, 22);
             this.Classroom_page.Name = "Classroom_page";
             this.Classroom_page.Padding = new System.Windows.Forms.Padding(3);
-            this.Classroom_page.Size = new System.Drawing.Size(670, 373);
+            this.Classroom_page.Size = new System.Drawing.Size(868, 373);
             this.Classroom_page.TabIndex = 3;
             this.Classroom_page.Text = "Classrooms";
             this.Classroom_page.UseVisualStyleBackColor = true;
             // 
-            // classroomPage1
-            // 
-            this.classroomPage1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.classroomPage1.Location = new System.Drawing.Point(3, 3);
-            this.classroomPage1.Name = "classroomPage1";
-            this.classroomPage1.Size = new System.Drawing.Size(664, 367);
-            this.classroomPage1.TabIndex = 0;
-            // 
             // tabPage3
             // 
+            this.tabPage3.Controls.Add(this.Refresh_Listbox_Teacher);
+            this.tabPage3.Controls.Add(this.Listbox_Teacher);
             this.tabPage3.Controls.Add(this.calendar1);
             this.tabPage3.Location = new System.Drawing.Point(4, 22);
             this.tabPage3.Name = "tabPage3";
             this.tabPage3.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage3.Size = new System.Drawing.Size(670, 373);
+            this.tabPage3.Size = new System.Drawing.Size(868, 373);
             this.tabPage3.TabIndex = 4;
             this.tabPage3.Text = "Calendar";
             this.tabPage3.UseVisualStyleBackColor = true;
             // 
             // calendar1
             // 
-            this.calendar1.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.calendar1.AllowDrop = true;
+            this.calendar1.Font = new System.Drawing.Font("Segoe UI", 11.25F);
             calendarHighlightRange1.DayOfWeek = System.DayOfWeek.Monday;
             calendarHighlightRange1.EndTime = System.TimeSpan.Parse("17:00:00");
             calendarHighlightRange1.StartTime = System.TimeSpan.Parse("08:00:00");
@@ -180,13 +153,69 @@
         calendarHighlightRange3,
         calendarHighlightRange4,
         calendarHighlightRange5};
-            this.calendar1.Location = new System.Drawing.Point(40, 31);
+            this.calendar1.Location = new System.Drawing.Point(40, 70);
             this.calendar1.Name = "calendar1";
-            this.calendar1.Size = new System.Drawing.Size(600, 325);
+            this.calendar1.Size = new System.Drawing.Size(600, 230);
             this.calendar1.TabIndex = 0;
             this.calendar1.Text = "calendar1";
             this.calendar1.ViewEnd = new System.DateTime(2015, 12, 11, 23, 59, 59, 0);
             this.calendar1.ViewStart = new System.DateTime(2015, 12, 4, 0, 0, 0, 0);
+            this.calendar1.LoadItems += new System.Windows.Forms.Calendar.Calendar.CalendarLoadEventHandler(this.calendar1_LoadItems_1);
+            this.calendar1.DragDrop += new System.Windows.Forms.DragEventHandler(this.calendar1_DragDrop);
+            this.calendar1.DragOver += new System.Windows.Forms.DragEventHandler(this.calendar1_DragOver);
+            // 
+            // Listbox_Teacher
+            // 
+            this.Listbox_Teacher.FormattingEnabled = true;
+            this.Listbox_Teacher.Location = new System.Drawing.Point(708, 31);
+            this.Listbox_Teacher.Name = "Listbox_Teacher";
+            this.Listbox_Teacher.Size = new System.Drawing.Size(127, 121);
+            this.Listbox_Teacher.TabIndex = 1;
+            this.Listbox_Teacher.SelectedIndexChanged += new System.EventHandler(this.Listbox_Teacher_SelectedIndexChanged);
+            this.Listbox_Teacher.DragEnter += new System.Windows.Forms.DragEventHandler(this.Listbox_Teacher_DragEnter);
+            this.Listbox_Teacher.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Listbox_Teacher_MouseDown);
+            // 
+            // Refresh_Listbox_Teacher
+            // 
+            this.Refresh_Listbox_Teacher.Location = new System.Drawing.Point(708, 159);
+            this.Refresh_Listbox_Teacher.Name = "Refresh_Listbox_Teacher";
+            this.Refresh_Listbox_Teacher.Size = new System.Drawing.Size(127, 23);
+            this.Refresh_Listbox_Teacher.TabIndex = 2;
+            this.Refresh_Listbox_Teacher.Text = "Refresh";
+            this.Refresh_Listbox_Teacher.UseVisualStyleBackColor = true;
+            this.Refresh_Listbox_Teacher.Click += new System.EventHandler(this.Refresh_Listbox_Teacher_Click);
+            // 
+            // promotion_page1
+            // 
+            this.promotion_page1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.promotion_page1.Location = new System.Drawing.Point(3, 3);
+            this.promotion_page1.Name = "promotion_page1";
+            this.promotion_page1.Size = new System.Drawing.Size(862, 367);
+            this.promotion_page1.TabIndex = 0;
+            // 
+            // teacher_page1
+            // 
+            this.teacher_page1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.teacher_page1.Location = new System.Drawing.Point(3, 3);
+            this.teacher_page1.Name = "teacher_page1";
+            this.teacher_page1.Size = new System.Drawing.Size(862, 367);
+            this.teacher_page1.TabIndex = 0;
+            // 
+            // students_page1
+            // 
+            this.students_page1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.students_page1.Location = new System.Drawing.Point(3, 3);
+            this.students_page1.Name = "students_page1";
+            this.students_page1.Size = new System.Drawing.Size(862, 367);
+            this.students_page1.TabIndex = 0;
+            // 
+            // classroomPage1
+            // 
+            this.classroomPage1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.classroomPage1.Location = new System.Drawing.Point(3, 3);
+            this.classroomPage1.Name = "classroomPage1";
+            this.classroomPage1.Size = new System.Drawing.Size(862, 367);
+            this.classroomPage1.TabIndex = 0;
             // 
             // header
             // 
@@ -194,14 +223,14 @@
             | System.Windows.Forms.AnchorStyles.Right)));
             this.header.Location = new System.Drawing.Point(0, 0);
             this.header.Name = "header";
-            this.header.Size = new System.Drawing.Size(703, 112);
+            this.header.Size = new System.Drawing.Size(901, 112);
             this.header.TabIndex = 0;
             // 
             // Main
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(720, 522);
+            this.ClientSize = new System.Drawing.Size(918, 522);
             this.Controls.Add(this.tabControl1);
             this.Controls.Add(this.header);
             this.Name = "Main";
@@ -230,6 +259,8 @@
         private ClassroomPage classroomPage1;
         private System.Windows.Forms.TabPage tabPage3;
         private System.Windows.Forms.Calendar.Calendar calendar1;
+        private System.Windows.Forms.ListBox Listbox_Teacher;
+        private System.Windows.Forms.Button Refresh_Listbox_Teacher;
     }
 }
 
--- SchoolIn/Base/Base/Form1.cs
+++ SchoolIn/Base/Base/Form1.cs
@@ -8,6 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using SchoolIn;
+using System.Windows.Forms.Calendar;
 
 namespace Base
 {
@@ -42,6 +43,59 @@ namespace Base
         private void calendar1_LoadItems(object sender, System.Windows.Forms.Calendar.CalendarLoadEventArgs e)
         {
 
+        }
+
+        private void Listbox_Teacher_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void Refresh_Listbox_Teacher_Click(object sender, EventArgs e)
+        {
+
+            Listbox_Teacher.Items.Clear();
+            foreach (var t in CurrentSchool.Teacher)
+            {
+                Listbox_Teacher.Items.Add(t.FirstName + " " + t.Name);
+            }
+        }
+
+        private void calendar1_LoadItems_1(object sender, System.Windows.Forms.Calendar.CalendarLoadEventArgs e)
+        {
+
+        }
+
+        private void Listbox_Teacher_MouseDown(object sender, MouseEventArgs e)
+        {
+            Listbox_Teacher.DoDragDrop(Listbox_Teacher.Text, DragDropEffects.Copy | DragDropEffects.Move);
+        }
+
+        private void Listbox_Teacher_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.Text))
+                e.Effect = DragDropEffects.Copy;
+            else
+                e.Effect = DragDropEffects.None;
+        }
+
+        private void calendar1_DragDrop(object sender, DragEventArgs e)
+        {
+           calendar1.Text = e.Data.GetData(DataFormats.Text).ToString();
+
+            // CalendarItem test1 = calendar1.ItemAt(calendar1.Bounds.Location);
+            // CalendarItem test3 = calendar1.ItemAt(calendar1.PointToClient(Cursor.Position));
+
+            Point Point = calendar1.PointToClient(new Point(e.X, e.Y));
+
+
+           ICalendarSelectableElement element = calendar1.HitTest(Point);
+           CalendarItem cal = new CalendarItem(calendar1, element.Date, element.Date.AddHours(2), calendar1.Text);
+           calendar1.Items.Add(cal);
+        }
+
+        private void calendar1_DragOver(object sender, DragEventArgs e)
+        {
+            e.Effect = DragDropEffects.Copy;
         }
     }
 }
