a1595d00f0fa0e2d53a9ddef9a7be2ff9fda7fda
Antoine Grandiere <grandiere.antoine@yahoo.com>
2015-12-16 10:04:52 +0100

    Ajout class course

---
+++ SchoolIn/SchoolIn/Course.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolIn
+{
+    [Serializable]
+    class Course
+    {
+        string _name;
+        List<Promotion> _listpromotion;
+        List<Teacher> _listteacher;
+        List<Classroom> _listclassroom;
+        School _school;
+        DateTime _start;
+        DateTime _end;
+
+        public Course(string name, School school)
+        {
+            if (string.IsNullOrWhiteSpace(name))
+            {
+                throw new ArgumentException();
+            }
+
+            _name = name;
+            _school = school;
+            _listteacher = new List<Teacher>();
+            _listpromotion = new List<Promotion>();
+            _listclassroom = new List<Classroom>();
+        }
+
+        public void AddPromotion(Promotion p)
+        {
+            if (p == null)
+                throw new NullReferenceException();
+
+            _listpromotion.Add(p);
+        }
+
+        public bool ExistPromotion(Promotion p)
+        {
+            if (p == null)
+                throw new NullReferenceException();
+
+            if (_listpromotion.Contains(p))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public bool RemovePromotion(Promotion p)
+        {
+            if (p == null)
+                throw new NullReferenceException();
+
+            string name = p.Name;
+
+            if (_listpromotion.Contains(p))
+            {
+                _listpromotion.Remove(p);
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public void AddClassroom(Classroom c)
+        {
+            if (c == null)
+                throw new NullReferenceException();
+
+            _listclassroom.Add(c);
+        }
+
+        public bool ExistClassroom(Classroom c)
+        {
+            if (c == null)
+                throw new NullReferenceException();
+
+            if (_listclassroom.Contains(c))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public bool RemoveClassroom(Classroom c)
+        {
+            if (c == null)
+                throw new NullReferenceException();
+
+            if (_listclassroom.Contains(c))
+            {
+                _listclassroom.Remove(c);
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public void AddTeacher(Teacher t)
+        {
+            if (t == null)
+                throw new NullReferenceException();
+
+            _listteacher.Add(t);
+        }
+
+        public bool ExistTeacher(Teacher t)
+        {
+            if (t == null)
+                throw new NullReferenceException();
+
+            if (_listteacher.Contains(t))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public bool RemoveTeacher(Teacher t)
+        {
+            if (t == null)
+                throw new NullReferenceException();
+
+            if (_listteacher.Contains(t))
+            {
+                _listteacher.Remove(t);
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+        public School School
+        {
+            get { return _school; }
+            set { _school = value; }
+        }
+        public DateTime Start
+        {
+            get { return _start; }
+            set { _start = value; }
+        }
+
+        public DateTime End
+        {
+            get { return _end; }
+            set { _end = value; }
+        }
+    }
+}
